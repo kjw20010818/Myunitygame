c5a2d6f0fec0fcd9849aac4a40b581bcaab8057c
kjw20010818 <kjw20010818@s.scnu.ac.kr>
2024-06-03 21:31:38 +0900

    run for bullet (ver 1.3)
    .

---
+++ Assets/Scrpits/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public float speed = 8f; // 총알 이동 속력
+    private Rigidbody bulletRigidbody; // 이동에 사용할 리지드바디 컴포넌트
+
+    void Start() {
+        // 게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 bulletRigidbody에 할당
+        bulletRigidbody = GetComponent<Rigidbody>();
+        // 리지드바디의 속도 = 앞쪽 방향 * 이동 속력
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        // 3초 뒤에 자신의 게임 오브젝트 파괴
+        Destroy(gameObject, 10f);
+    }
+
+    // 트리거 충돌 시 자동으로 실행되는 메서드
+}
